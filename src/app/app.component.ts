e3cbf33c4b06b4ec78bb71af562a5340e8dbadf9
r.kakaei <r.kakaei@irisaco.com>
2024-03-02 17:13:15 +0330

    install @types/three

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '3d-editor';
+
+
 }
